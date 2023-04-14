89c14b8a8c605dd7186dff59a3885edb7b21f0f4
Mbuford15 <104951451+Mbuford15@users.noreply.github.com>
2023-04-14 11:33:13 -0400

    Update getData.js

--- getData.js
+++ getData.js
@@ -2,7 +2,7 @@ var yourData;
 // gets the data from the spreadsheet
 // is currently being used as a back up just incase something breaks with the main one from altering it
 function getData () {
-    fetch("https://api.apispreadsheets.com/data/gcpeojQe6GyfvLZZ/").then(res => {
+    fetch("https://api.apispreadsheets.com/data/32PjRgplNbwQEvHy/").then(res => {
         if (res.status === 200) {
             //SUCCESS
             res.json().then(data => {
